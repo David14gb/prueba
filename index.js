daf6b9061b399742f56ff20cd6b4b360890d1cc1
David14gb <davidgarciab14@gmail.com>
2022-05-09 13:20:29 +0200

    cambios

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function sum(x,y) {
+    return x+y
+}
+
+function subs(x,y) {
+    return x-y
+}
+
+function mult(x,y) {
+    return x*y
+}
+
+function div(x,y) {
+    return x/y
+}
+
+function cuadrado(x) {
+    return Math.pow(x,2);
+}
+
+module.exports={sum, subs, mult, div, cuadrado};
